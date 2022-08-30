1511b5b8877a0fc4312b425383976fb6c56f34e6
seahwm <39093905+seahwm@users.noreply.github.com>
2022-08-30 10:45:14 +0800

    Add files via upload

---
+++ config.py
@@ -0,0 +1,6 @@
+customhost = "employee.cttcueef5wi2.us-east-1.rds.amazonaws.com"
+customuser = "aws_user"
+custompass = "Bait3273"
+customdb = "employee"
+custombucket = "seahweiming-employee"
+customregion = "us-east-1"
