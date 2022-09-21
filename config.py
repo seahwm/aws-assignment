10a862747e0302a3a2949571321016eccf4cfdb8
seahwm <seahwm01@gmail.com>
2022-09-21 20:08:58 +0800

    update database host

--- config.py
+++ config.py
@@ -1,4 +1,4 @@
-customhost = "employee.cttcueef5wi2.us-east-1.rds.amazonaws.com"
+customhost = "assignment-database.cttcueef5wi2.us-east-1.rds.amazonaws.com"
 customuser = "aws_user"
 custompass = "Bait3273"
 customdb = "employee"
